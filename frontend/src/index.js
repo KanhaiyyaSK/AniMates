da1790727723251f6ec439f476a640f4fe551302
KanhaiyyaSK <97464146+KanhaiyyaSK@users.noreply.github.com>
2023-07-19 22:09:11 +0530

    updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
 
 // to use the react alert everywhere in the program
-import { transitions, positions, Provider as AlertProvider } from 'react-alert'
-import AlertTemplate from 'react-alert-template-basic'
+import { transitions, positions, Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 const options = {
   timeout: 3000,
-  offset:'30px',
+  offset: "30px",
   position: positions.MIDDLE_RIGHT,
   transition: transitions.SCALE,
 };
@@ -18,7 +18,7 @@ const options = {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <AlertProvider  template={AlertTemplate} {...options}>
+    <AlertProvider template={AlertTemplate} {...options}>
       <App />
     </AlertProvider>
   </Provider>
