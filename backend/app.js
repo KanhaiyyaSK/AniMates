596189e98491ca9baf926e4ce6fecdbd5cebf93a
KanhaiyyaSK <97464146+KanhaiyyaSK@users.noreply.github.com>
2023-07-20 17:22:10 +0530

    fixing issues

--- backend/app.js
+++ backend/app.js
@@ -5,11 +5,9 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 
-
 const errorMiddleware = require("./middleware/error");
 const dotenv = require("dotenv");
 dotenv.config({ path: "backend/config/config.env" });
-
 
 app.use(express.json());
 app.use(morgan("dev"));
@@ -23,11 +21,14 @@ const user = require("./routes/userRoute");
 const order = require("./routes/orderRoute");
 const payment = require("./routes/paymentRoute");
 
+app.get("/", (req, res) => {
+  res.send("working");
+});
+
 app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
-
 
 // Middleware for Errors
 app.use(errorMiddleware);
